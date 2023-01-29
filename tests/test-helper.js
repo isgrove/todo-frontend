2bd3679766931727a849af9395fa24f4e2ea9287
sam isgrove <samis@hey.com>
2023-01-29 12:02:07 +1100

    initial commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'todo-frontend/app';
+import config from 'todo-frontend/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
